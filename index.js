45d9afeb1e812b76a853420cbbb910aca89177c5
derFinn <f.vehlhaber@web.de>
2017-10-10 10:58:42 +0200

    Set up basic server

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-var app = require('express')()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var config = require('./config')
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+const config = require('./config')
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+
+app.use(express.static(`${__dirname}/public`))
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`)
 })
 
+/*
 io.on('connection', function(socket) {
   socket.on('sendWifiSignalStrength', data => {
     io.emit('updateWifiSignalStrength', data)
   })
 })
+*/
 
-http.listen(config.port, function() {
-  console.log(`listening on *:${config.port}`)
+app.use((err, req, res, next) => {
+  if (!err) return res.sendFile(`${__dirname}/public/index.html`)
+  const errorMessage = `An error occured: ${err.message}\n${err.stack}\n`
+  process.stdout.write(errorMessage)
+  return res.status(500).json({ message: 'An unknown error occured.' })
+})
+
+server.listen(config.port, () => {
+  process.stdout.write(`Listening on *:${config.port}\n`)
 })
