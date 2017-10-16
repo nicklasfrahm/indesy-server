c0c257656802839629eb9e8135adac4971f5c000
nicklasfrahm <nicklas.frahm@gmail.com>
2017-10-16 19:52:06 +0200

    Seperate real-time API from REST API

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const socketio = require('socket.io')
-const http = require('http')
 const winston = require('winston')
 const config = require('./config')
 
 const app = express()
-const server = http.Server(app)
-const io = socketio(server, { path: '/ws' })
-const host = config.host || '0.0.0.0'
-const port = config.port || 8080
+const io = socketio({ path: '/ws' })
 
 let connections = 0
 let timer = 0
 
 winston.cli()
+winston.info(`[SIO] Listening on 0.0.0.0:${config.wsPort}`)
 
+// websocket server
 io.on('connection', function(connection) {
   const socket = connection
 
   ++connections
-  winston.info('[Server] A websocket client connected.')
+  winston.info('[SIO] A websocket client connected.')
   if (connections && !timer) {
     timer = setInterval(() => {
       socket.emit('testTimer', { timestamp: Date.now() })
@@ -28,7 +26,7 @@ io.on('connection', function(connection) {
 
   socket.on('disconnect', () => {
     --connections
-    winston.info('[Server] A websocket client disconnected.')
+    winston.info('[SIO] A websocket client disconnected.')
     if (!connections && timer) {
       clearInterval(timer)
       timer = null
@@ -36,6 +34,9 @@ io.on('connection', function(connection) {
   })
 })
 
+io.listen(config.wsPort)
+
+// express server
 app.get('/api/v1/hello-world', (req, res) => {
   return res.status(200).json({ message: 'Hello World!' })
 })
@@ -50,6 +51,6 @@ app.use((err, req, res, next) => {
   return res.status(500).json({ message: 'An unknown error occured.' })
 })
 
-server.listen(port, host, () => {
-  winston.info(`[Server] Listening on ${host}:${port}`)
+app.listen(config.apiPort, () => {
+  winston.info(`[API] Listening on 0.0.0.0:${config.apiPort}`)
 })
