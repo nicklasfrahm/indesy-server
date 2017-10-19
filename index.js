781647931ec2bc71179c46176dfc639505700304
nicklasfrahm <nicklas.frahm@gmail.com>
2017-10-19 22:26:02 +0200

    Improve logging and add environment variables

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express')
 const socketio = require('socket.io')
 const winston = require('winston')
+const http = require('http')
 
+const port = process.env.PORT || 8000
 const app = express()
-const io = socketio({
-  path: '/ws'
-})
-const config = {
-  wsPort: process.env.WS_PORT || 9000,
-  apiPort: process.env.API_PORT || 8000
-}
+const server = http.Server(app)
+const io = socketio(server, { path: '/sio' })
 
 let connections = 0
 let timer = 0
 
 winston.cli()
-winston.info(`[SIO] Listening on 0.0.0.0:${config.wsPort}`)
 
 // websocket server
 io.on('connection', function(connection) {
@@ -41,23 +37,24 @@ io.on('connection', function(connection) {
   })
 })
 
-io.listen(config.wsPort)
-
 // express server
 app.get('/api/v1/hello-world', (req, res) => {
   return res.status(200).json({ message: 'Hello World!' })
 })
 
 app.use((req, res, next) => {
+  winston.info(`[API] Unsupported request to: ${req.baseUrl}${req.url}`)
   return res
     .status(404)
     .jsonp({ message: 'This API endpoint is not supported.' })
 })
 
 app.use((err, req, res, next) => {
+  winston.error(`[API] ${err.message}\n${err.stack}`)
   return res.status(500).json({ message: 'An unknown error occured.' })
 })
 
-app.listen(config.apiPort, () => {
-  winston.info(`[API] Listening on 0.0.0.0:${config.apiPort}`)
+server.listen(port, () => {
+  winston.info(`[API] Listening on 0.0.0.0:${port}`)
+  winston.info(`[SIO] Listening on 0.0.0.0:${port}`)
 })
