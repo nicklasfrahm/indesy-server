bc727c890d7c94b0c19b8dcc34f375c55ae25f63
nicklasfrahm <nicklas.frahm@gmail.com>
2017-10-26 15:07:35 +0200

    Add test update emitter

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ loadControllers(app)
 
 // configure event handlers
 githubHookEmitter.on('*', (event, repo, payload) => {
+  io.to('robots').emit('update')
   const { ref } = payload
   const requiredRef = 'refs/heads/master'
   const requiredRepo = 'nicklasfrahm/indesy-robot'
