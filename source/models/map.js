8adb0def6916c698bf9772b694357cd082a66923
nicklasfrahm <nicklas.frahm@gmail.com>
2017-11-05 19:31:22 +0100

    Add map model

--- source/models/map.js
+++ source/models/map.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
 const MapSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true }
 })
