e0842e7aaa257013ff4c9a2053a2f137ec32b741
nicklasfrahm <nicklas.frahm@gmail.com>
2017-11-14 18:10:32 +0100

    Add case for id mismatch

--- source/controllers/robot.js
+++ source/controllers/robot.js
@@ -34,17 +34,32 @@ router.patch('/robots/:id', (req, res, next) => {
   if (!req.body) {
     return res.status(400).json({ error: 'The request body is empty.' })
   }
+  if (req.body._id && req.body._id !== req.params.id) {
+    return res
+      .status(400)
+      .json({ error: 'Payload ID does not match API path.' })
+  }
   if (req.body.token && !tokenRegex.test(req.body.token)) {
     crypto.randomBytes(16, (err, buffer) => {
       req.body.token = buffer.toString('hex')
       Model.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, doc) => {
         if (err) return next(err)
+        if (!doc) {
+          return res
+            .status(400)
+            .json({ error: 'There is no entity with this ID.' })
+        }
         return res.status(200).json(doc)
       })
     })
   } else {
     Model.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, doc) => {
       if (err) return next(err)
+      if (!doc) {
+        return res
+          .status(400)
+          .json({ error: 'There is no entity with this ID.' })
+      }
       return res.status(200).json(doc)
     })
   }
